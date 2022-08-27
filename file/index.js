692383683e7b85203154c4d9c1f98e075fc91ed8
AmetJunior <ametsejdaliev@gmail.com>
2022-08-27 18:22:38 +0300

    js

--- file/index.js
+++ file/index.js
@@ -2,7 +2,9 @@ console.log("Hello")
 
 const age = 35;
 if (age => 18) {
-    console.log("Вы совершено.лет-й")
+    function e() {
+        console.log("Error")
+    }
 
 } else {
     console.log("вы НЕ соверш-й")
