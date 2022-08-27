3d2bbe259e48a8bb7491f64873a6275577cf9f2c
AmetJunior <ametsejdaliev@gmail.com>
2022-08-27 17:58:25 +0300

    1234

--- file/index.js
+++ file/index.js
@@ -1,17 +1,10 @@
 console.log("Hello world")
 
-<<<<<<< HEAD
+
 const userAge = 35
 if(userAge => 18) {
     console.log("вЫ СОВЕРШ")
+    console.log("вЫ СОВЕРШ")
 } else {
     console.log("Вы не соверш")
-=======
-const age = 20;
-if (age => 18) {
-    console.log("Вы совершено.лет-й")
-
-} else {
-    console.log("вы НЕ соверш-й")
->>>>>>> feature/user-acess
 }
